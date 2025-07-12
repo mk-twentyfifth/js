8af237204d9d149c02420ca4d53f3660d749d7bb
mk-working <mk25.working@gmail.com>
2025-07-12 12:02:10 +0700

    clousure : WIP

---
+++ javascript/closure/lexical-scoping.js
@@ -0,0 +1,17 @@
+// why the displayName does not need the argument?
+
+// the var name & the fn displayName are 1 level under the init so it stay at the same lexical scoping, thus by the rules it can access the
+
+function init() {
+  // init - create - var
+  var name = "Mozilla";
+
+  // init - create - fn
+  function displayName() {
+    console.log(name);
+  }
+
+  // init - call -fn
+  displayName();
+}
+init();
---
+++ javascript/closure/scope-var.js
@@ -0,0 +1,9 @@
+if (Math.random() > 0.5) {
+  var x = "small";
+} else {
+  var x = "big";
+}
+
+console.log(x);
+
+// This can be tricky, because blocks with curly braces do not create scopes, unlike other languages (e.g., C, Java) where blocks create scopes
