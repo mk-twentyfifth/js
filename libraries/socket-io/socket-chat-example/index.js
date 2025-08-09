4e7df6047204c782017abbc6f268e172188c2a46
mk-working <mk25.working@gmail.com>
2025-08-09 11:56:14 +0700

    feat: socket-io -recovery

--- libraries/socket-io/socket-chat-example/index.js
+++ libraries/socket-io/socket-chat-example/index.js
@@ -5,6 +5,23 @@ import { createServer } from "node:http";
 import { fileURLToPath } from "node:url";
 import { dirname, join } from "node:path";
 import { Server } from "socket.io";
+import sqlite3 from "sqlite3";
+import { open } from "sqlite";
+
+// open the database file​(create db & connect)
+const db = await open({
+  filename: "chat.db",
+  driver: sqlite3.Database,
+});
+
+// create "message" table (you can ignore the "client_offset" column for now)
+await db.exec(`
+  CREATE TABLE IF NOT EXISTS messages (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    client_offset TEXT UNIQUE,
+    content TEXT
+    )
+  `);
 
 const app = express();
 const server = createServer(app);
@@ -18,11 +35,34 @@ app.get("/", (req, res) => {
   res.sendFile(join(__dirname, "index.html"));
 });
 
+// event listener for connection from client
 io.on("connection", (socket) => {
-  socket.on("chat message", (msg) => {
-    io.emit("chat message", msg);
+  // event listener
+  socket.on("chat message", async (msg) => {
+    let result;
+    try {
+      result = await db.run("INSERT INTO messages (content) VALUES (?)", msg);
+    } catch (error) {
+      console.log(error);
+    }
+
+    io.emit("chat message", msg, result.lastID);
   });
 });
+
+if (!socket.recovered) {
+  try {
+    await db.each(
+      `SELECT id, content FROM messages WHERE id > ?`,
+      [socket.handshake.auth.serverOffset || 0],
+      (_err, row) => {
+        socket.emit("chat message", row.content, row.id);
+      }
+    );
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 server.listen(3001, () => {
   console.log("server running at http://localhost:3001");
